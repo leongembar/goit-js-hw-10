fb48f36b12c3fab4d95e453f2dfad8481bb1c98f
Леонід Гембаровський <leongembar@gmail.com>
2023-09-23 03:29:58 +0300

    2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,58 @@
-import axios from "axios";
+import {fetchBreeds, fetchCatByBreed} from "./js/cat-api";
 
-axios.defaults.headers.common["x-api-key"] = "live_H7U1N5cgm9uvYmVTabhiqFJPMgW8iLwIs9tSOcUFn1agAedsNaWWVcfIqWIA1Gqh";
+
+const refs = {
+    selectCat: document.querySelector('.breed-select'),
+    catCart: document.querySelector(".cat-info"),
+    loader: document.querySelector(".loader"),
+    error: document.querySelector(".error")
+}
+
+
+
+fetchBreeds().then((data) => {
+    refs.selectCat.classList.remove('select-hidden');
+    refs.loader.classList.add('loader-hidden');
+    renderSelect(data, refs.selectCat);
+    
+});
+
+
+function renderSelect(arr, container){
+   const markupList = arr.map(
+        ({id, name}) =>
+            `<option value="${id}">${name}</option>`
+        ).join("");
+    container.innerHTML = markupList;
+}
+
+
+refs.selectCat.addEventListener("change", (e) => {
+    refs.catCart.classList.add('cat-hidden');   
+    refs.loader.classList.remove('loader-hidden');
+    fetchCatByBreed(e.target.value)
+    .then(({breeds, url})=>{
+        rendeerCard(breeds[0].name , breeds[0].description, breeds[0].temperament, url, refs.catCart);
+        refs.loader.classList.add('loader-hidden');
+        refs.catCart.classList.remove('cat-hidden'); 
+    }
+        
+    );
+  });
+
+  function rendeerCard(name, description, temperament, url, container){
+    const markupCart = 
+            `
+            <img src="${url}" width="400" alt=${name} />
+            <div class="text-cont">
+            <h2>${name}</h2>
+            <p>${description}</p>
+            <p>${temperament}</p>
+            </div>
+            
+            `
+        ;
+    container.innerHTML = markupCart;
+  }
+
 
---
+++ src/js/cat-api.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+axios.defaults.headers.common["x-api-key"] = "live_H7U1N5cgm9uvYmVTabhiqFJPMgW8iLwIs9tSOcUFn1agAedsNaWWVcfIqWIA1Gqh";
+
+
+export function fetchBreeds(){
+    return axios.get('https://api.thecatapi.com/v1/breeds')
+  .then(function (response) {
+    return response.data;
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
+export function fetchCatByBreed(breedId){
+    return axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+  .then(function (response) {
+    // console.log(response.data[0])
+    return response.data[0];
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
